d4d13c3326a58d27b3bc48eb49a5c4c37cba25e0
Larissa Aparecida <larissa8senezio@gmail.com>
2023-05-11 15:40:07 -0300

    react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <p> <img src='/src/assets/img/icon.png' alt="Imagem Tela Inicial" className="img"/> </p>
+        </>
+    );
+}
+
+export default Home;
