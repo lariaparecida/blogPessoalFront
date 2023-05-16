88562986aca5a2660e705aedd26f3654821493d3
Larissa Aparecida <larissa8senezio@gmail.com>
2023-05-16 15:16:30 -0300

    links

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
-import React from 'react';
 import Navbar from './components/estaticos/navbar/Navbar';
-import Footer from './components/estaticos/footer/Footer';
-import {Grid} from '@material-ui/core'
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
+import Footer from './components/estaticos/footer/Footer';
+import { BrowserRouter , Routes , Route } from 'react-router-dom'
 import "./App.css";
 
 function App() {
   return (
-    <>
+
+  <>
+    <BrowserRouter> 
       <Navbar />
-      <Home />
+        <div style={{ minHeight: '100vh' }}>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+          </Routes>
+        </div>
       <Footer />
-    </>
+    </BrowserRouter>
+  </>
+  // <>
+  //   <Router> -- atual BroswerRouter
+  //     <Navbar />  -- mantém        
+  //         <Switch> -- Novo Routes
+  //           <div> -- a nova vem antes do Route/Switch
+  //              <Route path='/home'>
+  //                 <Home />
+  //              </Route>
+  //           </div>
+  //         </Switch>
+  //     <Footer />
+  //   </Router>
+  // </>
   );
 }
 
--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
-//import React from 'react';
 import { AppBar, Toolbar, Typography, Box} from '@material-ui/core';
+import {Link} from 'react-router-dom';
+import './Navbar.css';
 
 
 function Navbar () {
@@ -8,43 +9,46 @@ function Navbar () {
             <AppBar position="static">
         <Toolbar variant="dense">
 
-            <Box style={{ cursor: "pointer" }} >
+            <Box className='cursor' >
                 <Typography variant="h5" color="inherit">
-                    BlogPessoal
+                    Blog Pessoal
                 </Typography>
             </Box>
 
         <Box display="flex" justifyContent="start">
 
-                    <Box mx={1} style={{ cursor: "pointer" }}>
+                    <Box mx={1} className='cursor'>
                         <Typography variant="h6" color="inherit">
                             Home
                         </Typography>
                     </Box>
                 
-                    <Box mx={1} style= {{ cursor: "pointer" }}>
+                    <Box mx={1} className='cursor'>
                         <Typography variant="h6" color="inherit">
                             Postagens
                         </Typography>
                     </Box>
 
-                    <Box mx={1} style= {{ cursor: "pointer" }}>
+                    <Box mx={1} className='cursor'>
                         <Typography variant="h6" color="inherit">
                             Temas
                         </Typography>
                     </Box>
 
-                    <Box mx={1} style= {{ cursor: "pointer" }}>
+                    <Box mx={1} className='cursor'>
                         <Typography variant="h6" color="inherit">
                             Cadastrar tema
                         </Typography>
                     </Box>
 
-                    <Box mx={1} style= {{ cursor: "pointer" }}>
-                        <Typography variant="h6" color="inherit">
-                            Logout
-                        </Typography>
-                    </Box>
+                    <Link to='/login' className='text-decorator-none'>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Logout
+                            </Typography>
+                        </Box>
+                    </Link>
+                    
         </Box>
 
         </Toolbar>
